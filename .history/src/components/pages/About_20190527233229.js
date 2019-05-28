b62087213ee3cef69e09f89978a182ad5cba1dd9
Mike <troianello@protonmail.com>
2019-05-27 23:33:38 -0400

    updated about page

--- .history/src/components/pages/About_20190527233229.js
+++ .history/src/components/pages/About_20190527233229.js
@@ -6,9 +6,9 @@ export default function About() {
       <h1 className="display-4">About Contact Manager</h1>
       <p className="lead">
         This is a simple app to manage dummy contacts. It is a full CRUD
-        application, created by Mike Troianello in{" "}
-        <span className="text-danger">React</span>.
+        application, created with React.
       </p>
+      <p>Created by Mike Troianello</p>
       <p className="text-secondary">Version 1.0.0</p>
     </div>
   );
